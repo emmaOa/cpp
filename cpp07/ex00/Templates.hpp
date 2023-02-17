8a04ccf362f2a7cd4793ddcd23b3ad2b2f481735
emmaOa <imaneouazzani.pro@gmail.com>
2023-02-18 00:21:07 +0100

    cpp07-ex00

--- cpp07/ex00/Templates.hpp
+++ cpp07/ex00/Templates.hpp
@@ -2,14 +2,14 @@
 # define TEMPLATES_HPP
 # include <iostream>
 
-template <typename T> T max(T x, T y)
+template <typename T> T max(T &x, T &y)
 {
-    return (x >= y ? x : y);
+    return (x > y ? x : y);
 }
 
-template <typename T> T min(T x, T y)
+template <typename T> T min(T &x, T &y)
 {
-    return (x > y ? y : x);
+    return (x < y ? x : y);
 }
 
 template <typename T> void swap(T &x, T &y)
