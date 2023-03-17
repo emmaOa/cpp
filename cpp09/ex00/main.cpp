f1bad7d5665a80b838df189457941c75649b102a
emmaOa <imaneouazzani.pro@gmail.com>
2023-03-17 20:33:58 +0100

    cpp09-ex00-check date

--- cpp09/ex00/main.cpp
+++ cpp09/ex00/main.cpp
@@ -6,41 +6,134 @@
 /*   By: iouazzan <iouazzan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/14 16:50:43 by iouazzan          #+#    #+#             */
-/*   Updated: 2023/03/16 19:48:59 by iouazzan         ###   ########.fr       */
+/*   Updated: 2023/03/17 20:32:03 by iouazzan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "BitcoinExchange.hpp"
 
 
-date_ln *check_date(std::string dt)
+int dt_to_int(date_ln *date, int tst, std::string str)
 {
-    date_ln *date = new date_ln();
-    size_t i = 0;
-    std::string str;
-    
-    date->v = -1;
-    std::cout <<"---->>" << dt << std::endl;
-
-    while (dt[i] == ' ' || dt[i] == '\t')
-        i++;
-    while (dt[i] != '-' && i < dt.length())
+    if (tst == 0)
     {
-        if (dt[i] < '0' || dt[i] > '9')
+        try{
+            date->y = atof(str.c_str());
+        }
+        catch (const std::invalid_argument & e) {
+            std::cout << "Error: bad input [d :0 - 3]=> " << str << std::endl;
+            return -1;
+        }
+
+        catch (const std::out_of_range & e) {
+            std::cout << "Error: too large a number [d : 0 - 4]." << "\n";
+            return -1;
+        }
+    }
+    else if (tst == 1)
+    {
+
+        try{
+            date->m = atof(str.c_str());
+        }
+        catch (const std::invalid_argument & e) {
+            std::cout << "Error: bad input [d : 1 - 3]=> " << str << std::endl;
+            return -1;
+        }
+
+        catch (const std::out_of_range & e) {
+            std::cout << "Error: too large a number [d : 1 - 4]." << "\n";
+            return -1;
+        }  
+    }
+    else
+    {
+        try{
+            date->d = atof(str.c_str());
+        }
+        catch (const std::invalid_argument & e) {
+            std::cout << "Error: bad input [d : 2 - 3]=> " << str << std::endl;
+            return -1;
+        }
+
+        catch (const std::out_of_range & e) {
+            std::cout << "Error: too large a number [d : 2 - 4]." << "\n";
+            return -1;
+        }
+    }
+    return 1;
+}
+
+int check_date_utl(date_ln *date, size_t i, int if_d, std::string dt)
+{
+    std::string str;
+    while (i < (dt.length() - 1) && (dt[i] != '-' || (dt[i] != ' ' && if_d == 2)))
+    {
+        if ((dt[i] < '0' || dt[i] > '9'))
         {
             std::cout << "Error: bad input [d : 1]=> " << dt << std::endl;
-            return date;
+            return 0;
         }
         str += dt[i];
         i++;
     }
-    std::cout << dt[i - 1] << "\n";
-    if (i == dt.length() || dt[i] != '-' || (dt[i] == '-' && dt[i - 1] == '-'))
+    if (dt[i] != '-')
     {
-        std::cout << "Error: bad input [d : 2]=> " << dt << std::endl;
+        if (if_d == 0 || if_d == 1)
+        {
+            std::cout << "Error: bad input [d : 2]=> " << dt << std::endl;
+            return 0;
+        }
+        else{
+            if (dt[i] != ' ')
+            {
+                std::cout << "Error: bad input [d : 3]=> " << dt << std::endl;
+                return 0;
+            }
+            if (dt_to_int(date, if_d, str) < 1)
+                return 0;
+        }
+    }
+    else{
+        if (dt_to_int(date, if_d, str) < 1)
+            return 0;
+    }
+    return i;
+}
+
+date_ln *check_date(std::string dt)
+{
+    date_ln *date = new date_ln();
+    size_t i = 0;
+    
+    date->v = -1;
+    i = check_date_utl(date, i, 0, dt);
+    if (i == 0)
+        return date;
+    if (date->y <  2009 || date->y >  2011)
+    {
+        std::cout << "Error: bad input [d : 5]=> " << date->y << std::endl;
         return date;
     }
-    
+    i++;
+    i = check_date_utl(date, i, 1, dt);
+    if (i == 0)
+        return date;
+    if (date->m <  1 || date->m >  12)
+    {
+        std::cout << "Error: bad input [d : 6]=> " << date->m << std::endl;
+        return date;
+    }
+    i++;
+    i = check_date_utl(date, i, 2, dt);
+    if (i == 0)
+        return date;
+    if (date->d <  1 || date->d >  30)
+    {
+        std::cout << "Error: bad input [d : 7]=> " << date->m << std::endl;
+        return date;
+    }
+    date->v = 1;
     return date;
 
 }
@@ -100,7 +193,7 @@ pair *check_line(std::string line)
         return pr;
     }
     try{
-        pr->value =atof(nb.c_str());
+        pr->value = atof(nb.c_str());
     }
     catch (const std::invalid_argument & e) {
         std::cout << "Error: bad input => " << line << std::endl;
@@ -182,7 +275,7 @@ int main(int arc, char *arv[])
     //     std::cout<<it->first<<" "<<it->second<< std::endl;
     // }
         //  exit(0);
-    
+    date_ln *date;
     pair *pr2;
     std::ifstream file;
     file.open(arv[1]);
@@ -198,7 +291,13 @@ int main(int arc, char *arv[])
         if (pr2->valide == 1)
         {
             // std::cout<<pr2->key<<" "<<pr2->value<< std::endl;
-            check_date(pr2->key);
+            date = check_date(pr2->key);
+            if (date->v == 1)
+            {
+                
+                // std::cout << date->y << "-" << date->m << "-" << date->d << "\n";
+                std::cout << pr2->key << "\n";
+            }
         }
     }
     file.close();
